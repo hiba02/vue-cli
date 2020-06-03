864b0a1270856cb2b1487d7230d40cdf610a6994
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-06-03 13:25:42 -0400

    initial setup and home component

---
+++ src/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <app-server-status v-for="sever in 5"></app-server-status>
+  </div>
+</template>
+
+<script>
+import ServiceStatus from "./ServiceState.vue";
+export default {
+  components: {
+    "app-server-status": ServiceStatus
+  }
+};
+</script>
+
+<style></style>
---
+++ src/ServiceState.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <hr />
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: "Critical"
+    };
+  },
+  methods: {
+    //ES6 expression
+    changeStatus() {
+      this.status = "Normal";
+    }
+  }
+};
+</script>
